aa3ac3400e5ab281afb4ca81502d7e7ee09d3481
pxjaizq <pojai71688@gmail.com>
2022-07-04 11:39:04 +0700

    first commit

---
+++ bin/generetion.dart
@@ -0,0 +1,18 @@
+void main() {
+  int gen = 2005;
+  if(gen >= 2013 && gen <= 2025){
+    print("Gen: Alpha ");
+  }else if(gen >= 1997 && gen <= 2012){
+    print("Gan: Z");
+  }else if(gen >= 1981 && gen <= 1996){
+    print("Gen: Y");
+  }else if(gen >= 1965 && gen <= 1964){
+    print("Gen: X");
+  }else if(gen >= 1928 && gen <= 1945){
+    print("Gen: Baby Boomer");
+  }else if(gen >= 1928 && gen <= 1945){
+    print("Gen: Silent");
+  }else{
+    print("ERROR");
+  }
+}
